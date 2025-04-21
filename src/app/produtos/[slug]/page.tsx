a5d90b80929a02c835805457d8e31d036aa82cc6
joao <joaovitormvbr@gmail.com>
2025-04-21 16:53:43 -0300

    slug com array

--- src/app/produtos/[slug]/page.tsx
+++ src/app/produtos/[slug]/page.tsx
@@ -1,41 +1,51 @@
-// app/produtos/[slug]/page.tsx
 import DressDetail from '@/components/DressDetail';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { notFound } from 'next/navigation';
 
-export default function DressDetailPage({ params }: { params: { slug: string } }) {
-  // Dados fictícios com imagens locais
-  const dresses = [
-    {
-      id: 1,
-      name: 'Vestido vitoria',
-      price: 'R$ 399,90',
-      description:
-        'Um vestido elegante com detalhes brilhantes, perfeito para ocasiões especiais. Confeccionado com tecidos de alta qualidade, garante conforto e sofisticação.',
-      frontImage: '/images/vitoria.jpg',
-      backImage: '/images/vitoria-verso.jpg',
-      slug: 'vestido-vitoria',
-    },
-  ];
+// Definir tipo do vestido
+interface Dress {
+  id: number;
+  name: string;
+  price: string;
+  description: string;
+  frontImage: string;
+  backImage: string;
+  slug: string;
+}
 
-  const dress = dresses.find((d) => d.slug === params.slug);
+// Array local de vestidos
+const dresses: Dress[] = [
+  {
+	id: 1,
+	name: 'Vestido Vitoria',
+	price: 'R$ 399,90',
+	description:
+  	'Um vestido elegante com detalhes brilhantes, perfeito para ocasiões especiais. Confeccionado com tecidos de alta qualidade, garante conforto e sofisticação.',
+	frontImage: '/images/vitoria.jpg',
+	backImage: '/images/vitoria-verso.jpg',
+	slug: 'vestido-vitoria',
+  },
+]
+
+// Definir tipo dos props de forma flexível
+type tParams = Promise<{ slug: string }>;
+
+export async function generateStaticParams() {
+  return dresses.map((dress) => ({ slug: dress.slug }));
+}
+
+export default async function DressDetailPage({ params }: { params: tParams }) {
+  // Resolver params, caso seja um Promise
+  const { slug }: { slug: string } = await params;
+  // Buscar o vestido pelo slug no array local
+  const dress = dresses.find((d) => d.slug === slug);
+
+  // console.log('params:', params, typeof params);
+  // console.log('resolvedParams:', resolvedParams);
 
   if (!dress) {
-    return (
-      <div className="flex flex-col min-h-screen bg-[#FFFFFF]">
-        <header>
-          <Navbar />
-        </header>
-        <main className="flex-grow flex items-center justify-center">
-          <p className="font-[Poppins-light] text-xl text-[#641311]">
-            Ops, Vestido não encontrado 
-          </p>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    );
+    notFound(); // Aciona a página 404 do Next.js
   }
 
   return (
@@ -51,4 +61,6 @@ export default function DressDetailPage({ params }: { params: { slug: string } }
       </footer>
     </div>
   );
+  
 }
+  
